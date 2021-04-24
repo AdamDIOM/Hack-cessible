109b1ebcf58c54182c73335e7b102a45485293df
AdamDIOM <adam.drummond9@gmail.com>
2021-04-24 22:58:51 +0100

    added finished version

--- texttospeech.py
+++ texttospeech.py
@@ -1,2 +1,27 @@
 import os
-os.system("say hello world")
+import requests
+#os.system("say hello world")
+
+
+def ocr_space_file(filename, overlay=False, api_key='helloworld', language='eng'):
+
+    payload = {'isOverlayRequired': overlay,
+               'apikey': api_key,
+               'language': language,
+               }
+    with open(filename, 'rb') as f:
+        r = requests.post('https://api.ocr.space/parse/image',
+                          files={filename: f},
+                          data=payload,
+                          )
+    return r
+
+print("Enter image file path")
+
+text = ocr_space_file(input()).json()
+
+print(text['ParsedResults'][0]['ParsedText'].replace("\n", " . ").replace("\r", " . "))
+for line in text['ParsedResults'][0]['ParsedText'].split():
+    os.system("say " + line)
+
+os.system("say finished")
